6549040bc86049900cc7a2cec6f451cd06ed31ca
Faroo, Mohamed Javeed <Mohamed.Javeed.Faroo@dell.com>
2018-02-24 01:06:10 +0000

    Travis build integration

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import uuid
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
 
-__author__ = 'Senthil Kumar Ganesan <skg.dev.net@gmail.com>'
+__author__ = 'Mohamed Javeed <javeedf@gmail.com>'
 
 install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
 reqs = [str(ir.req) for ir in install_reqs]
@@ -14,8 +14,8 @@ setup(
     name="napalm-dellos10",
     version="0.1.0",
     packages=find_packages(),
-    author="Senthil Kumar Ganesan",
-    author_email="skg.dev.net@gmail.com",
+    author="Mohamed Javeed",
+    author_email="javeedf@gmail.com",
     description="NAPALM driver for Dell EMC Networking OS10 Operating System.",
     classifiers=[
         'Topic :: Utilities',
