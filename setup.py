eb815838017bb529da5d17faa55f8bc17faab085
javeedf <Mohamed_Javeed_Faroo@dell.com>
2020-05-28 18:05:23 +0530

    napalm 3.0.1 cpmpatalility fix

--- setup.py
+++ setup.py
@@ -1,19 +1,12 @@
 """setup.py file."""
 
-import uuid
+from setuptools import setup, find_packages
 
-try:  # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError:  # for pip <= 9.0.3
-    from pip.req import parse_requirements
-
-from setuptools import find_packages, setup
-
+with open("requirements.txt", "r") as fs:
+    reqs = [r for r in fs.read().splitlines()
+            if (len(r) > 0 and not r.startswith("#"))]
 
 __author__ = 'Mohamed Javeed <javeedf.dev@gmail.com>'
-
-install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
-reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name="napalm-dellos10",
@@ -25,10 +18,10 @@ setup(
     classifiers=[
         'Topic :: Utilities',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Operating System :: POSIX :: Linux',
         'Operating System :: MacOS',
     ],
